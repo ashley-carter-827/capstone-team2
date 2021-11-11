57c3928fc66034d592e43acf38df071fbfd2ea13
Debra Mella <debra.mella@libertymutual.com>
2021-11-11 17:11:36 -0600

    Changed Schedule to Appointment in model, route, and controller filews.

---
+++ routes/appointment.routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+//middleware used to require authentication
+const { validateJwtMiddleware } = require("../auth");
+
+//import the appointment controller to handle our user routes
+const appointmentController = require("../controllers/appointment.controller")
+
+//post route to create a appointment 
+router.post("/", validateJwtMiddleware, appointmentController.createAppointment)
+
+//get route to return all appointments (requires auth)
+router.get("/", validateJwtMiddleware, appointmentController.getAppointment)
+
+//put route to update a appointment (requires auth)
+router.put("/:email", validateJwtMiddleware, appointmentController.updateAppointment)
+
+module.exports = router;
